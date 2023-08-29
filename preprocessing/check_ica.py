be1bef852e5bfed1ad00df4ebbd170c91cfb823a
laurabpaulsen <202005791@post.au.dk>
2023-08-29 16:56:48 +0200

    add scripts

---
+++ preprocessing/check_ica.py
@@ -0,0 +1,8 @@
+"""
+The main purpose of the code in this python script is to:
+
+    1) preprocess the raw data (high and low pass filter, exclude bad channels, interpolate bad channels)
+    2) plot the ICA solutions generated in 'run_ica.py' so noise components can be identified.
+
+Noise components should be added to session_info.py
+"""
---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,4 @@
+"""
+This script preprocesses the data after identifying bad channels, time span of the experiment and identifying noise components
+
+"""
