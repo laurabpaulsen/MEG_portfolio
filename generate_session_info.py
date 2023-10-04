8ac42bfa7f21a4f6209c1bce11b99eb1aad8fa10
laurabpaulsen <202005791@post.au.dk>
2023-10-04 17:28:47 +0200

    upload generator script for session info

---
+++ generate_session_info.py
@@ -0,0 +1,60 @@
+"""
+Saves dictionary to pickle file containing session information (bad channels, noise components, time span of experiment)
+"""
+
+from pathlib import Path
+import pickle
+
+
+session_info = {
+    "0115": {
+        "self_block1.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "self_block2.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "self_block3.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block3.fif": {
+            "bad_channels": [], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+
+}
+
+
+if __name__ == "__main__":
+    # Save session_info dictionary to pickle file
+    with open(Path(__file__).parent / "session_info.pickle", "wb") as f:
+        pickle.dump(session_info, f)
