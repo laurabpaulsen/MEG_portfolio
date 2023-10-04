a0034a157a0be47202712d061ab54ad5be5c7634
laurabpaulsen <202005791@post.au.dk>
2023-10-04 17:32:55 +0200

    change to notebook

--- preprocessing/check_raw.py
+++
@@ -1,7 +0,0 @@
-"""
-This python script is used to plot the raw data to;
-    1) identify bad channels
-    2) identify the beginning and the end of the experiment
-
-Remember to add the channels, tmin and tmax to event_session_info.py. Remember to run event_session_info.py after changing the values.
-"""
