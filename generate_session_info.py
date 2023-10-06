769b676f78f64a5d80bb9fb05a884ca37371a281
laurabpaulsen <202005791@post.au.dk>
2023-10-06 12:38:28 +0200

    update session info with reject

--- generate_session_info.py
+++ generate_session_info.py
@@ -1,13 +1,13 @@
 """
-Saves dictionary to pickle file containing session information (bad channels, noise components, time span of experiment)
+Saves dictionary to txt file containing session information (bad channels, noise components, time span of experiment, reject criterion)
 """
 
 from pathlib import Path
-import pickle
-
+import json
 
 session_info = {
     "0115": {
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 200e-6},
         "self_block1.fif": {
             "bad_channels": ["MEG0422"], 
             "tmin": 0, 
@@ -51,10 +51,240 @@ session_info = {
         },
     },
 
+    "0114": {
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 300e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "self_block3.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block3.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+    "0113":{
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 400e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+
+    "0112":{
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 400e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+
+    "0111":{
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 500e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+    "0110":{
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 300e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+
+    "0109":{
+        "reject":{"mag": 3000e-15, "grad": 3000e-13, "eog": 400e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    },
+
+    "0108":{
+        "reject": {"mag": 3000e-15, "grad": 3000e-13, "eog": 400e-6},
+        "self_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block1.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": ["MEG0422"]
+        },
+
+        "self_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+
+        "other_block2.fif": {
+            "bad_channels": ["MEG0422"], 
+            "tmin": 0, 
+            "tmax": 0, 
+            "noise_components": []
+        },
+    }
+
+    
+
 }
 
 
-if __name__ == "__main__":
-    # Save session_info dictionary to pickle file
-    with open(Path(__file__).parent / "session_info.pickle", "wb") as f:
-        pickle.dump(session_info, f)
+
+with open(Path(__file__).parent / "session_info.txt", "w") as f:
+    f.write(json.dumps(session_info))
