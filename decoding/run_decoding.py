999922f01ad90451180070622fb5e33255030080
laurabpaulsen <202005791@post.au.dk>
2023-10-08 12:37:25 +0200

    add paths

---
+++ decoding/run_decoding.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+
+if __name__ in "__main__":
+    path = Path(__file__).parent
+
+    data_path = path / "data"
+
+    outpath = path / "results"
+
+    # create output directory if it doesn't exist
+    if not outpath.exists():
+        outpath.mkdir()
+    
+    subjects = ["0108", "0109", "0110", "0111", "0112", "0113", "0114", "0115"]
+
+    
