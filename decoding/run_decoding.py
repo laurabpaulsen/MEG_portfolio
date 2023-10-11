dccbcf3f430bc0c2604fd0e9602a978554af57c4
laurabpaulsen <202005791@post.au.dk>
2023-10-11 16:14:51 +0200

    decoding files

--- decoding/run_decoding.py
+++ decoding/run_decoding.py
@@ -6,7 +6,6 @@ if __name__ in "__main__":
     path = Path(__file__).parent
 
     data_path = path / "data"
-
     outpath = path / "results"
 
     # create output directory if it doesn't exist
@@ -14,5 +13,6 @@ if __name__ in "__main__":
         outpath.mkdir()
     
     subjects = ["0108", "0109", "0110", "0111", "0112", "0113", "0114", "0115"]
+    
 
     
