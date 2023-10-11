3a82d47fe73d2a5801facbe496b5231d9e27d456
laurabpaulsen <202005791@post.au.dk>
2023-10-11 10:42:31 +0200

    move function to utils

--- preprocessing/prep_for_classification.py
+++ preprocessing/prep_for_classification.py
@@ -5,124 +5,15 @@ This script loops over all participants and all sessions and prepares the data f
 
 """
 
+import sys
 from pathlib import Path
-import mne
-import numpy as np
 import json
+import mne
 
+# local imports
+sys.path.append(str(Path(__file__).parents[1]))
+from utils import preprocess_data_sensorspace, epochs_to_sourcespace, morph_stcs_label
 
-def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None, ica_path:Path = None, noise_components = None):
-    """
-    
-    Parameters
-    ----------
-    fif_path : Path
-        Path to the fif file.
-    bad_channels : list
-        List of bad channels.
-    reject : dict, optional
-        Reject dictionary. The default is None.
-    ica_path : Path, optional
-        Path to the ICA file. The default is None.
-    noise_components : list, optional
-        List of noise ICA components. The default is None.
-    
-    Returns
-    -------
-    epochs : mne.Epochs
-        Epochs object.
-    """
-    raw = mne.io.read_raw_fif(fif_path, preload = True)
-
-    # projecting out the empty room noise
-    raw.apply_proj()
-
-    # Low pass filtering to get rid of line noise
-    raw.filter(0.1, 40, n_jobs = 4)
-
-    if ica_path:
-        ica = mne.preprocessing.read_ica(ica_path)
-
-        # remove noise components
-        ica.exclude = noise_components
-
-        # apply ica
-        ica.apply(raw)
-
-    # find the events
-    events = mne.find_events(raw, min_duration=2/raw.info["sfreq"])
-
-    # remove bad channels
-    raw.drop_channels(bad_channels)
-
-    # epoching
-    epochs = mne.Epochs(raw, events, tmin=-0.2, tmax=1, baseline=(None, 0), preload = True, reject = reject)
-
-    # downsampling
-    epochs.resample(250)
-    
-    return epochs
-
-
-def epochs_to_sourcespace(epochs, fwd,  pick_ori='normal', lambda2=1.0 / 9.0, method='dSPM', label=None):
-    """
-    Parameters
-    ----------
-    epochs : mne.Epochs
-        Epochs object.
-    fwd : mne.Forward
-        Forward solution.
-    pick_ori : str, optional
-        Orientation of the inverse solution. The default is 'normal'.
-    lambda2 : float, optional
-        Regularization parameter. The default is 1.0 / 9.0.
-    method : str, optional
-        Inverse method. The default is 'dSPM'.
-    label : mne.Label, optional
-        Label to restrict the inverse solution to. The default is None.
-    
-    Returns
-    -------
-    stcs : list
-        List of source time courses.
-    """
-    noise_cov = mne.compute_covariance(epochs, tmax=0.000)
-    
-    inv = mne.minimum_norm.make_inverse_operator(epochs.info, fwd, noise_cov)
-
-    stcs = mne.minimum_norm.apply_inverse_epochs(epochs, inv, lambda2, method, label, pick_ori=pick_ori)
-    
-    return stcs
-
-def morph_stcs_label(morph_path:Path, stcs:list, fs_subjects_dir:Path, label_regexp:str = 'parsopercularis-lh'):
-    """
-    Parameters
-    ----------
-    morph_path : Path
-        Path to the morph file.
-    stcs : list
-        List of source time courses to morph.
-    fs_subjects_dir : Path
-        Path to the freesurfer subjects directory.
-    label_regexp : str, optional
-        Regular expression to select the label. The default is 'parsopercularis-lh'.
-    
-    Returns
-    -------
-    X : np.array
-        Array with source time courses.
-    """
-    morph = mne.read_source_morph(morph_path)
-            
-    # morph from subject to fsaverage
-    stcs = [morph.apply(stc) for stc in stcs]
-
-    label = mne.read_labels_from_annot("fsaverage", parc="aparc", subjects_dir=fs_subjects_dir, regexp=label_regexp)[0]
-    vertices = label.get_vertices_used(stcs[0].vertices[0]) # get sources from the data that are within the label
-            
-    X = np.array([stc.data[vertices, :] for stc in stcs])
-
-    return X
 
 
 
@@ -151,8 +42,6 @@ def main():
 
     for subject in subjects:
         subject_info = session_info[subject]
-        reject = subject_info["reject"]
-
         subject_path = MEG_data_path / subject
         
         # find the folder with MEG data and not the folder with MRI data
@@ -171,7 +60,7 @@ def main():
 
             ICA_path_sub = ICA_path / subject / f"{recording_name}-ica.fif"
 
-            epochs = preprocess_data_sensorspace(fif_file_path, reject, subject_session_info["bad_channels"], ICA_path_sub, subject_session_info["noise_components"])
+            epochs = preprocess_data_sensorspace(fif_file_path, subject_session_info["bad_channels"], subject_info["reject"], ICA_path_sub, subject_session_info["noise_components"])
 
             # load forward solution
             fwd_fname = recording_name[4:] + '-oct-6-src-' + '5120-fwd.fif'
---
+++ utils.py
@@ -0,0 +1,117 @@
+
+import mne
+import numpy as np
+from pathlib import Path
+
+def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None, ica_path:Path = None, noise_components = None):
+    """
+    
+    Parameters
+    ----------
+    fif_path : Path
+        Path to the fif file.
+    bad_channels : list
+        List of bad channels.
+    reject : dict, optional
+        Reject dictionary. The default is None.
+    ica_path : Path, optional
+        Path to the ICA file. The default is None.
+    noise_components : list, optional
+        List of noise ICA components. The default is None.
+    
+    Returns
+    -------
+    epochs : mne.Epochs
+        Epochs object.
+    """
+    raw = mne.io.read_raw_fif(fif_path, preload = True)
+
+    # projecting out the empty room noise
+    raw.apply_proj()
+
+    # Low pass filtering to get rid of line noise
+    raw.filter(0.1, 40, n_jobs = 4)
+
+    if ica_path:
+        ica = mne.preprocessing.read_ica(ica_path)
+
+        # remove noise components
+        ica.exclude = noise_components
+
+        # apply ica
+        ica.apply(raw)
+
+    # find the events
+    events = mne.find_events(raw, min_duration=2/raw.info["sfreq"])
+
+    # remove bad channels
+    raw.drop_channels(bad_channels)
+
+    # epoching
+    epochs = mne.Epochs(raw, events, tmin=-0.2, tmax=1, baseline=(None, 0), preload = True, reject = reject)
+
+    # downsampling
+    epochs.resample(250)
+    
+    return epochs
+
+
+def epochs_to_sourcespace(epochs, fwd,  pick_ori='normal', lambda2=1.0 / 9.0, method='dSPM', label=None):
+    """
+    Parameters
+    ----------
+    epochs : mne.Epochs
+        Epochs object.
+    fwd : mne.Forward
+        Forward solution.
+    pick_ori : str, optional
+        Orientation of the inverse solution. The default is 'normal'.
+    lambda2 : float, optional
+        Regularization parameter. The default is 1.0 / 9.0.
+    method : str, optional
+        Inverse method. The default is 'dSPM'.
+    label : mne.Label, optional
+        Label to restrict the inverse solution to. The default is None.
+    
+    Returns
+    -------
+    stcs : list
+        List of source time courses.
+    """
+    noise_cov = mne.compute_covariance(epochs, tmax=0.000)
+    
+    inv = mne.minimum_norm.make_inverse_operator(epochs.info, fwd, noise_cov)
+
+    stcs = mne.minimum_norm.apply_inverse_epochs(epochs, inv, lambda2, method, label, pick_ori=pick_ori)
+    
+    return stcs
+
+def morph_stcs_label(morph_path:Path, stcs:list, fs_subjects_dir:Path, label_regexp:str = 'parsopercularis-lh'):
+    """
+    Parameters
+    ----------
+    morph_path : Path
+        Path to the morph file.
+    stcs : list
+        List of source time courses to morph.
+    fs_subjects_dir : Path
+        Path to the freesurfer subjects directory.
+    label_regexp : str, optional
+        Regular expression to select the label. The default is 'parsopercularis-lh'.
+    
+    Returns
+    -------
+    X : np.array
+        Array with source time courses.
+    """
+    morph = mne.read_source_morph(morph_path)
+            
+    # morph from subject to fsaverage
+    stcs = [morph.apply(stc) for stc in stcs]
+
+    label = mne.read_labels_from_annot("fsaverage", parc="aparc", subjects_dir=fs_subjects_dir, regexp=label_regexp)[0]
+    vertices = label.get_vertices_used(stcs[0].vertices[0]) # get sources from the data that are within the label
+            
+    X = np.array([stc.data[vertices, :] for stc in stcs])
+
+    return X
