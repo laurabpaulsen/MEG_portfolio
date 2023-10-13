6f43a5450eef7d4728451881b40288401624fda9
laurabpaulsen <202005791@post.au.dk>
2023-10-13 13:28:01 +0200

    ICA plot

--- preprocessing/ica_example_plot.py
+++ preprocessing/ica_example_plot.py
@@ -14,43 +14,56 @@ def plot_noise_components(ica, raw, ica_noise_components, outpath):
     Plots the noise components of an ICA object.
     """
     fig = plt.figure(figsize=(10, 7))
-    spec = GridSpec(4, 2, width_ratios=[1, 2])
+    spec = GridSpec(5, 2, width_ratios=[0.7, 2])
 
     # Create subplots using the grid
-    topomap_axes = [plt.Subplot(fig, spec[i, 0]) for i in range(4)]
-    time_series_axes = [plt.Subplot(fig, spec[i, 1]) for i in range(4)]
+    topomap_axes = [plt.Subplot(fig, spec[i, 0]) for i in range(5)]
+    time_series_axes = [plt.Subplot(fig, spec[i, 1]) for i in range(5)]
 
-    for i in range(4):
+    for i in range(5):
         fig.add_subplot(topomap_axes[i])
         fig.add_subplot(time_series_axes[i])
 
     # Plot the topomap of the components
-    ica.plot_components(inst=raw, picks=ica_noise_components, axes=topomap_axes, show=False)
+    ica.plot_components(inst=raw, picks=ica_noise_components[:2], axes=topomap_axes[:2], show=False)
+    ica.plot_components(inst=raw, picks=ica_noise_components[-1], axes=topomap_axes[-2], show=False)
+    
     for ax in topomap_axes:
         title = ax.get_title()
         ax.set_title("")
-        ax.set_ylabel(title)
+        ax.set_ylabel(title[:-6])
 
+
+    topomap_axes[2].axis("off")
     topomap_axes[-1].axis("off")
 
     # Plot the time series of the components
     freq = raw.info["sfreq"]
     ica_sources = ica.get_sources(raw).get_data(start=int(20*freq), stop=int(30*freq))[ica_noise_components]
-    extra_sensors = raw.get_data(['EOG001', 'EOG002', 'ECG003'], start=int(20*freq), stop=int(30*freq))
+    extra_sensors = raw.get_data(['EOG001', 'ECG003'], start=int(20*freq), stop=int(30*freq))
 
     # Plot the time series of the components
-    for idx, ax in enumerate(time_series_axes[:3]):
+    for idx, ax in enumerate(time_series_axes[:2]):
         ax.plot(ica_sources[idx].T, linewidth=0.5, color="k")
+    
+    time_series_axes[-2].plot(ica_sources[-1].T, linewidth=0.5, color="k")
 
-    time_series_axes[-1].plot(extra_sensors[2].T, label="ECG", linewidth=0.5, color="darkgreen")
-    time_series_axes[-1].plot(extra_sensors[0].T, label="EOG", linewidth=0.5, color="darkblue")
-    time_series_axes[-1].legend()
+    time_series_axes[2].plot(extra_sensors[1].T, label="ECG", linewidth=0.5, color="#0047AB")
+    time_series_axes[-1].plot(extra_sensors[0].T, label="EOG", linewidth=0.5, color="#0047AB")
+    time_series_axes[-1].legend(loc = "upper right")
+    time_series_axes[2].legend(loc = "lower right")
+
+    for ax in time_series_axes:
+        ax.set_xticks(np.arange(0, 2501, step=250), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        ax.set_xlim(0, 2501)
 
     # set labels
     topomap_axes[0].set_title("Topomaps")
     time_series_axes[0].set_title("Timeseries")
 
+    
 
+    fig.tight_layout()
     plt.savefig(outpath / "ica_components.png")
 
 
