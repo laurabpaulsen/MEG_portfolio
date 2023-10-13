5e630f4942126152f9c54e4479e7e41adaa1f720
zeyus <zeyus@zeyus.com>
2023-10-13 10:51:21 +0200

    support multiprocessing, fix multi-label

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import mne
 import numpy as np
 from pathlib import Path
 
-def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None, ica_path:Path = None, noise_components = None, event_ids = None):
+def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None, ica_path:Path = None, noise_components = None, event_ids = None, n_jobs = 4):
     """
     
     Parameters
@@ -30,7 +30,7 @@ def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None,
 
 
     # Low pass filtering to get rid of line noise
-    raw.filter(0.1, 40, n_jobs = 4)
+    raw.filter(0.1, 40, n_jobs = n_jobs)
 
     if ica_path:
         ica = mne.preprocessing.read_ica(ica_path)
@@ -51,12 +51,12 @@ def preprocess_data_sensorspace(fif_path:Path, bad_channels:list, reject = None,
     epochs = mne.Epochs(raw, events, event_id = event_ids, tmin=-0.2, tmax=1, baseline=(None, 0), preload = True, reject = reject, proj = True)
 
     # downsampling
-    epochs.resample(250)
+    epochs.resample(250, n_jobs = n_jobs)
     
     return epochs
 
 
-def epochs_to_sourcespace(epochs, fwd,  pick_ori='normal', lambda2=1.0 / 9.0, method='dSPM', label=None, return_inv = False):
+def epochs_to_sourcespace(epochs, fwd,  pick_ori='normal', lambda2=1.0 / 9.0, method='dSPM', label=None, n_jobs = 4):
     """
     Parameters
     ----------
@@ -78,16 +78,14 @@ def epochs_to_sourcespace(epochs, fwd,  pick_ori='normal', lambda2=1.0 / 9.0, me
     stcs : list
         List of source time courses.
     """
-    noise_cov = mne.compute_covariance(epochs, tmax=0.000)
+    noise_cov = mne.compute_covariance(epochs, tmax=0.000, n_jobs=n_jobs)
     
     inv = mne.minimum_norm.make_inverse_operator(epochs.info, fwd, noise_cov)
 
     stcs = mne.minimum_norm.apply_inverse_epochs(epochs, inv, lambda2, method, label, pick_ori=pick_ori)
-    if return_inv:
-        return stcs, inv
     
-    else:
-        return stcs
+    return stcs
+
 def morph_stcs_label(morph_path:Path, stcs:list, fs_subjects_dir:Path, label_regexp:str = 'parsopercularis-lh'):
     """
     Parameters
@@ -111,50 +109,9 @@ def morph_stcs_label(morph_path:Path, stcs:list, fs_subjects_dir:Path, label_reg
     # morph from subject to fsaverage
     stcs = [morph.apply(stc) for stc in stcs]
 
-    label = mne.read_labels_from_annot("fsaverage", parc="aparc", subjects_dir=fs_subjects_dir, regexp=label_regexp)[0]
-    vertices = label.get_vertices_used(stcs[0].vertices[0]) # get sources from the data that are within the label
+    labels = mne.read_labels_from_annot("fsaverage", parc="aparc", subjects_dir=fs_subjects_dir, regexp=label_regexp)
+    vertices = np.concatenate([label.get_vertices_used(stcs[0].vertices[0]) for label in labels])
             
     X = np.array([stc.data[vertices, :] for stc in stcs])
 
     return X
-
-
- 
-def flip_sign(X1, X2):
-    """
-    This function is used to flip the sign of the data in X2 if the correlation between the data in X1 and X2 is negative.
-
-    Parameters
-    ----------
-    X1 (array): 
-        Data from the session to compare to with shape (n_trials, n_channels, n_times)
-        
-    X2 (array): 
-        Data from the session to flip the sign of with shape (n_trials, n_channels, n_times)
-
-    Returns
-    -------
-    X2 (ndarray): 
-        X2 with the sign flipped if the correlation between X1 and X2 is negative in the given parcel with shape (n_channels, n_trials, n_times)
-    """
-
-    # checking that the T and P dimensions are the same
-    if X1.shape[2] != X2.shape[2]:
-        raise ValueError('The number of time points in the two sessions are not the same')
-    
-    if X1.shape[1] != X2.shape[1]:
-        raise ValueError('The number of parcels in the two sessions are not the same')
-
-    # loop over parcels
-    for i in range(X1.shape[1]):
-        # take means over trials
-        mean1 = np.mean(X1[:, i, :], axis = 0)
-        mean2 = np.mean(X2[:, i, :], axis = 0)
-
-        # calculate correlation
-        corr = np.corrcoef(mean1, mean2)[0, 1]
-
-        if corr < 0: # if correlation is negative, flip sign
-            X2[:, i, :] = X2[:, i, :] * -1
-
-    return X2
