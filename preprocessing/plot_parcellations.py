66932b053b35e2d1a9bcb18f79d9eba6356b4ae0
laurabpaulsen <202005791@post.au.dk>
2023-10-18 21:32:51 +0200

    try something else

--- preprocessing/plot_parcellations.py
+++ preprocessing/plot_parcellations.py
@@ -10,22 +10,48 @@ import matplotlib.pyplot as plt
 
 from pathlib import Path
 
-def plot_parc(subject, parc, hemi, surf_name, subjects_dir, axes):
-    brain = mne.viz.Brain(
+# local imports
+import sys
+sys.path.append(str(Path(__file__).parents[1]))
+from utils import preprocess_data_sensorspace, epochs_to_sourcespace, morph_stcs_label
+
+def plot_parc(stc, subject, parc, hemi, surf_name, subjects_dir, ax):
+    mne.vis.plot_source_estimates(
+        stc,
         subject=subject,
+        surface=surf_name,
         hemi=hemi,
-        surf=surf_name,
         subjects_dir=subjects_dir,
-        background="white",
-        foreground="black",
-        cortex="bone",
-        size=(800, 600),
-        show=False,
-        axes=axes)
-    
-    brain.add_annotation(
-        parc=parc
+        time_label="",
+        colormap="viridis",
+        transparent=True,
+        axes=ax,
+        colorbar=False,
+        show=False
     )
+
+
+def get_stc(fif_file_path, fwd, label, fs_subjects_dir, subject):
+    epochs = preprocess_data_sensorspace(
+                    fif_path = fif_file_path, 
+                    bad_channels = None, 
+                    event_ids={"img/self/positive": 11, "img/self/negative": 12},
+                    tmin = -0.2,
+                    tmax = 2)
+
+    # load forward solution
+    fwd_fname = recording_name[4:] + '-oct-6-src-' + '5120-fwd.fif'
+    fwd = mne.read_forward_solution(fs_subjects_dir / subject / 'bem' / fwd_fname)
+
+    # get source time courses
+    stcs = epochs_to_sourcespace(epochs, fwd)
+
+    # morph from subject to fsaverage
+    morph_subject_path = fs_subjects_dir / subject / "bem" / f"{subject}-oct-6-src-morph.h5"
+                
+    morphed = morph_stcs_label(morph_subject_path, stcs, fs_subjects_dir, label)
+
+    return morphed
 
 
 
@@ -33,27 +59,21 @@ if __name__ == "__main__":
     path = Path(__file__).parents[1]
 
     freesurfer_path = Path("/work/835482")
+    subject = "0108"
+    recording_name = '001.self_block1'
+    MEG_data_path = Path("/work/834761")
+    fwd_fsaverage_path = freesurfer_path / "fsaverage" / "bem" / "fsaverage-oct-6-src.fif"
 
-    # Load the data
-    area1 = mne.read_labels_from_annot(
-        subject="fsaverage",
-        parc="aparc",
-        hemi="lh",
-        surf_name="white",
-        subjects_dir=freesurfer_path
-    )
+    subject_path = MEG_data_path / subject
+            
+    # find the folder with MEG data and not the folder with MRI data
+    subject_meg_path = list(subject_path.glob("*_000000"))[0]
 
-    area2 = mne.read_labels_from_annot(
-        subject="fsaverage",
-        parc="aparc",
-        hemi="rh",
-        surf_name="white",
-        subjects_dir=freesurfer_path
-    )
+    # get the 
+    fif_file_path = list((subject_meg_path / "MEG" / recording_name / "files").glob("*.fif"))[0]
 
-    # Plot the data
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    plot_parc("fsaverage", "aparc", "lh", "white", freesurfer_path, axes[0])
-    plot_parc("fsaverage", "aparc", "rh", "white", freesurfer_path, axes[1])
+    morped = get_stc(fif_file_path, fwd_fsaverage_path, "superiorfrontal-rh", freesurfer_path, "fsaverage")
 
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    plot_parc(morped, "fsaverage", "superiorfrontal-rh", "rh", "pial", freesurfer_path, ax)
     plt.savefig(path / "fig" / "parcellations.png")
