bde02b2877b7be49a3c824fba02326832ab496db
laurabpaulsen <202005791@post.au.dk>
2023-10-18 17:42:01 +0200

    prep for longer epochs

--- decoding/plot_results.py
+++ decoding/plot_results.py
@@ -6,7 +6,7 @@ def axis_seconds(ax):
     """
     Changes the x axis to seconds
     """
-    ax.set_xticks(np.arange(0, 301, step=50), [-0.2, 0. , 0.2, 0.4, 0.6, 0.8, 1. ])
+    ax.set_xticks(np.arange(0, 550), step=50), [-0.2, 0. , 0.2, 0.4, 0.6, 0.8, 1. , 1.2, 1.4, 1.6, 1.8, 2.])
 
 def plot_decoding_accuracy(acc, title, legend_title, savepath = None):
     fig, ax = plt.subplots(1, 1, figsize = (12, 8), dpi = 300)
@@ -34,7 +34,7 @@ def plot_decoding_accuracy(acc, title, legend_title, savepath = None):
     ax.legend(title = legend_title, loc = "upper right")
 
     # x limits
-    ax.set_xlim(0, 300)
+    ax.set_xlim(0, 250*2.2)
 
     if savepath:
         plt.savefig(savepath)
@@ -46,34 +46,24 @@ if __name__ in "__main__":
     results_path = path / "results" 
     outpath = path / "fig"
 
-    label = "whole_brain"
+    x_files = ["across_subjects_pos_neg_area_1.npy", 
+               "across_subjects_pos_neg_area_2.npy",
+               "across_subjects_assigned_selfchosen_area_1.npy",
+               "across_subjects_assigned_selfchosen_area_2.npy"]
+    
 
     # create output directory if it doesn't exist
     if not outpath.exists():
         outpath.mkdir()
 
-    acc = np.load(results_path / f"across_subjects_11_202_{label}.npy")
-
-    plot_decoding_accuracy(
-        acc, 
-        title = "Decoding accuracy for self chosen positive and button press (across subject)", 
-        legend_title = "Test subject",
-        savepath = outpath / f"across_subjects_11_202_{label}.png"
-        )
-
-
-    within_subject = []
-    for i in range(1, 9):
-        within_subject.append(np.load(results_path / f"within_subject_{i}_11_202_{label}.npy"))
-
-    within_subject = np.mean(np.array(within_subject), axis = 2)
-    
-    plot_decoding_accuracy(
-        within_subject,
-        legend_title = "Subject",
-        title = "Decoding accuracy for self chosen positive and button press (within subject)", 
-        savepath = outpath / f"within_subjects_11_202_{label}.png"
-        )
+    for files in x_files:
+        acc = np.load(results_path / files)
+        plot_decoding_accuracy(
+            acc, 
+            title = files, 
+            legend_title = "Test subject",
+            savepath = outpath / f"{files[:-4]}.png"
+            )
 
     
 
