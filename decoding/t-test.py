ae16098e0dfdceb204e4bb15be13324b9365cfc0
laurabpaulsen <202005791@post.au.dk>
2023-10-20 13:20:50 +0200

    fixed

--- decoding/t-test.py
+++ decoding/t-test.py
@@ -16,7 +16,7 @@ if __name__ in "__main__":
     time_windows = [(.520, .670)] # add more time windows here (in seconds)
 
     # account for the fact that we have data from -0.2 seconds before the onset of the stimulus
-    time_windows = [(-0.2 + tw[0], -0.2 + tw[1]) for tw in time_windows]
+    time_windows_new = [(-0.2 + tw[0], -0.2 + tw[1]) for tw in time_windows]
     decoding_types = ["pos_neg", "assigned_selfchosen"]
     areas = ["LIFG", "mPFC"]
 
@@ -24,7 +24,7 @@ if __name__ in "__main__":
     with open(results_path / f"t-test.txt", "w") as f:
         f.write("")
 
-    for tw in time_windows:
+    for i, tw in enumerate(time_windows_new):
         for area in areas:
             acc_dt1 = np.load(results_path / f"across_subjects_{decoding_types[0]}_area_{area}_150.npy")
             acc_dt2 = np.load(results_path / f"across_subjects_{decoding_types[1]}_area_{area}_150.npy")
@@ -46,7 +46,7 @@ if __name__ in "__main__":
 
             # write results to file
             with open(results_path / f"t-test.txt", "a") as f:
-                f.write(f"Time window: {tw}\n")
+                f.write(f"Time window: {time_windows[i]}\n")
                 f.write(f"Decoding type 1: {decoding_types[0]}\n")
                 f.write(f"Decoding type 2: {decoding_types[1]}\n")
                 f.write(f"Mean accuracy decoding type 1: {np.mean(acc_dt1)}, standard deviation: {np.std(acc_dt1)}\n")
